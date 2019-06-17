ed8b407a6f5f34c8806dee44b9f59d241568fdf6
Doz Lau <liuxiaoliang@vlifepaper.com>
2019-06-17 14:19:32 +0800

    删除多余的ts内容

--- src/pages/result/result.ts
+++ src/pages/result/result.ts
@@ -1,56 +1,9 @@
-import {pagify, MyPage, wxp} from 'base/'
-import {Admin} from './../../utils/admin'
-
+import {pagify, MyPage} from 'base/'
 @pagify()
 export default class extends MyPage {
     data = {
-        userData: {},
-        phone: '4009210610'
-    }
 
+    }
     async onLoad(options: any) {
-        // console.log(await wxp.getUserInfo())
-    }
-
-    async onShow() {
-        let that = this
-        let userData = Admin.default().userData
-
-        that.setDataSmart({
-            userData: userData
-        })
-    }
-
-    getPhoneNumber(e: any) {
-        if (e.detail.errMsg == 'getPhoneNumber:ok') {
-
-        }
-    }
-
-    async call() {
-        let that = this
-
-        await wxp.makePhoneCall({
-            phoneNumber: that.data.phone
-        })
-    }
-
-    async toEditInfo() {
-        wx.navigateTo({
-                url: '../userdata/userdata'
-            }
-        )
-    }
-
-    async clickCell1() {
-        wx.navigateTo({url: '../history/history'})
-    }
-
-    async clickCell2() {
-        console.log('ceshidwe2');
-    }
-
-    async clickCell3() {
-        console.log('ceshidwe3');
     }
 }
