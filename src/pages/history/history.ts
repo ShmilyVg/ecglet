4aafae49f7b8527bb76293a3caadc5a0df00dded
李小云 <lixiaoyun@vlifepaper.com>
2019-06-17 18:02:53 +0800

    完善了检测记录页面

--- src/pages/history/history.ts
+++ src/pages/history/history.ts
@@ -8,12 +8,6 @@ import Toast from '../../base/heheda-common-view/toast.js';
 import * as tools from "../../utils/tools";
 import Protocol from '../../apis/network/protocol.js'
 
-/*type Log = {
-    id: number,
-    localfile: string,
-    rawdata: any,
-    createdTime: string
-}*/
 @pagify()
 export default class extends MyPage {
     data = {
@@ -26,19 +20,15 @@ export default class extends MyPage {
         console.log(this.data.logs)
     }
 
-    /*onShow() {
-        this.getList({page: 1});
-        console.log(this.data.logs)
-    }*/
-
     getList({page = 1, recorded = false}){
         Toast.showLoading();
-        Protocol.getHistoryList({ page }).then(data =>{
+        Protocol.getHistoryList({ page }).then((data) =>{
             let list = data.result.dataList
-            for (let i = 0;  i < list.length; i++) {
-                const {date, time} = tools.createDateAndTime(list[i].time);
-                console.log({date, time})
-            }
+            list.forEach(item=>{
+                const {date, time} = tools.createDateAndTime(parseInt(item.time));
+                item.date = date;
+                item.time = time;
+            })
             this.setData({
                 logs : list
             })
