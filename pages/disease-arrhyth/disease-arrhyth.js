d0b67a8f631a0fe89090cacaf733967c7c7b6375
Doz Lau <liuxiaoliang@vlifepaper.com>
2019-07-02 17:33:36 +0800

    导入「疾病介绍」类

--- pages/disease-arrhyth/disease-arrhyth.js
+++ pages/disease-arrhyth/disease-arrhyth.js
@@ -1,11 +1,6 @@
-// 此文件是由模板文件 ".dtpl/page/$rawModuleName.ts.dtpl" 生成的，你可以自行修改模板
+Page({
+    data: {},
 
-import {pagify, MyPage} from 'base/'
-
-@pagify()
-export default class extends MyPage {
-    data = {}
-
-    async onLoad(options: any) {
+    async onLoad() {
     }
-}
+})
