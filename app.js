7bd1f8a368dc04ce148c000c1bf34209d1ba2aaa
liubiao <liubiao@vlifepaper.com>
2019-07-17 11:18:05 +0800

    重定向

--- app.js
+++ app.js
@@ -7,40 +7,47 @@ import UserInfo from "./apis/network/network/libs/userInfo";
 import HiNavigator from "./components/navigator/hi-navigator";
 
 App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    console.log('qqq')
-    wx.onNetworkStatusChange((res)=> {
-      console.log('网络状态变更', res);
-      this.globalData.isConnected = res.isConnected;
-      const currentPages = getCurrentPages();
-      if (currentPages && currentPages.length) {
-        const pageListener = currentPages[currentPages.length - 1].onNetworkStatusChanged;
-        console.log('页面监听函数', pageListener);
-        pageListener && pageListener(res);
-      }
-      // }
-    });
+    onLaunch: function () {
+        // 展示本地存储能力
+        console.log('qqq')
+        wx.onNetworkStatusChange((res) => {
+            console.log('网络状态变更', res);
+            this.globalData.isConnected = res.isConnected;
+            const currentPages = getCurrentPages();
+            if (currentPages && currentPages.length) {
+                const pageListener = currentPages[currentPages.length - 1].onNetworkStatusChanged;
+                console.log('页面监听函数', pageListener);
+                pageListener && pageListener(res);
+            }
+            // }
+        });
 
-    Login.doLogin().then((data) => {
-      return UserInfo.get();
-    }).then((res) => {
-      console.log('app getUserInfo',res);
-      wx.setStorageSync('isRegister', true);
-      if (!wx.getStorageSync('phoneNumber')) {
-        wx.setStorageSync('phoneNumber', res.userInfo.phone || '');
-      }
-      this.onLoginSuccess && this.onLoginSuccess();
-    }).catch((res) => {
-      console.log('app.js login fail',res);
-      if (res && res.data && res.data.code === 2) {
-        HiNavigator.relaunchToWelcome();
-      }
-    });
-  },
-  globalData: {
-    userInfo: {}, tempGatherResult: {}, isConnected: true
-  },
-  onLoginSuccess: null,
+        Login.doLogin().then((data) => {
+            return UserInfo.get();
+        }).then((res) => {
+            console.log('app getUserInfo', res);
+            wx.setStorageSync('isRegister', true);
+            const {phone, birthday} = res.userInfo;
+            if (!wx.getStorageSync('phoneNumber')) {
+                wx.setStorageSync('phoneNumber', res.userInfo.phone || '');
+            }
+            if (!phone || !birthday) {
+                HiNavigator.relaunchToWelcome();
+            } else {
+                this.onLoginSuccess && this.onLoginSuccess();
+            }
+        }).catch((res) => {
+            console.log('app.js login fail', res);
+            if (res && res.data && res.data.code === 2) {
+                HiNavigator.relaunchToWelcome();
+            }
+        });
+    },
+
+    globalData: {
+        userInfo: {}, tempGatherResult: {}, isConnected: true
+    },
+
+    onLoginSuccess: null,
 
 });
