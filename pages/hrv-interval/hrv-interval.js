81de0b89321ca809e88585349502e6697fe9f2c4
liubiao <liubiao@vlifepaper.com>
2019-08-15 11:42:37 +0800

    完成心率变异性

--- pages/hrv-interval/hrv-interval.js
+++ pages/hrv-interval/hrv-interval.js
@@ -18,26 +18,21 @@ Page({
     Protocol.getHrvInterval({id: this.dataId}).then(data => {
       const {result, result: {frequency}} = data;
       let fre = parseInt(frequency) || 0, titleColor = '', position = 0, maxNum = 150, iconWidth = 2;//iconWidth=2 是2%的意思
-      const {lineNum, lineColor} = this.data;
+      const {lineNum} = this.data;
       if (fre <= lineNum[0]) {
         position = (0.25 / lineNum[0] * fre * 100 - iconWidth).toFixed(3) + '%';
-        titleColor = lineColor[0].color;
       } else if (fre <= lineNum[1]) {
         position = (25 + 0.25 / (lineNum[1] - lineNum[0]) * (fre - lineNum[0]) * 100 - iconWidth).toFixed(3) + '%';
-        titleColor = lineColor[1].color;
       } else if (fre <= lineNum[2]) {
         position = (50 + 0.25 / (lineNum[2] - lineNum[1]) * (fre - lineNum[1]) * 100 - iconWidth).toFixed(3) + '%';
-        titleColor = lineColor[2].color;
       } else {
         position = (75 + 0.25 / (maxNum - lineNum[2]) * (fre - lineNum[2]) * 100 - iconWidth);
         position = position <= (100 - iconWidth) ? position : 100 - iconWidth;
         position = position.toFixed(3) + '%';
-        titleColor = lineColor[3].color;
       }
 
       this.setData({
         position,
-        titleColor,
         result
       });
     });
