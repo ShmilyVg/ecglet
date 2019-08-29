3a4a9301316e653cfc92d940480d226f7086493a
liubiao <liubiao@vlifepaper.com>
2019-08-29 10:37:34 +0800

    平均心率UI

--- pages/avg-hr/avg-hr.js
+++ pages/avg-hr/avg-hr.js
@@ -5,11 +5,11 @@ Page({
 
     data: {
         lineColor: [
-            {color: '#4BABEA', text: '严重心动过缓'},
-            {color: '#5ACD4E', text: '心动过缓'},
-            {color: '#FDC400', text: '正常'},
-            {color: '#FC7648', text: '轻度心动过速'},
-            {color: '#EF3A59', text: '心动过速'}
+            {color: '#4B87EA', text: '严重心动过缓'},
+            {color: '#4BABEA', text: '心动过缓'},
+            {color: '#5ACD4E', text: '正常'},
+            {color: '#FDC400', text: '轻度心动过速'},
+            {color: '#F75161', text: '心动过速'}
         ],
         lineNum: [50, 60, 90, 100]
     },
